29949a8abe0a4fb16f88b94c84f9b3792f8d4d54
milansap <sapkota111milan@gmail.com>
2022-11-09 12:06:53 +0545

    first commit

---
+++ Admin/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/admin.css">
+    <title>Admin panel </title>
+</head>
+<body>
+    <!-- menu sec. starts -->
+    <section class="menu-bar ">
+        <div class="wrapper">
+            <div class="logo">
+                <img src="../images/logo.png" class="img-responsive" alt="logo">
+            </div>
+            <div class="menu ">
+                <ul>
+                    <li class="text-center"><a href="#">Dashboard</a></li>
+                    <li><a href="#">Users</a></li>
+                    <li><a href="#">Category</a></li>
+                    <li><a href="#">Product</a></li>
+                    <li><a href="#">Order</a></li>
+                </ul>
+            </div>
+            <div class="clearfix"></div>
+            <div class="form-wrapper">
+                <form action=""></form>
+            </div>
+        </div>
+    </section>
+    <!-- menu end here -->
+    <!-- body sec. starts -->
+    <section class="content">
+        <div class="wrapper">
+        <div class="breadcumb">
+            <ul>
+                <li><a href="#">Dashboard</a></li>
+            
+            </ul>
+        </div>
+        </div>
+    </section>
+    <!-- body end here -->
+    <!-- footer sec. starts -->
+    <section class="footer">
+        <div class="wrapper text-center">
+            <p>Copyright <?php echo date('Y');?>. </p>
+            <p>All Rights Reserved. Designed and developed by  <a  class="footer-link" href="#">Milan Sapkota</a> </p>
+            
+        </div>
+    </section>
+    <!-- menu end here -->
+</body>
+</html>
